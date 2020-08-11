37733ed1e102f778aace6668c9982a1b094b16a3
Marten <marten.frisk@gmail.com>
2020-08-11 16:59:24 +0200

    Updates

---
+++ precache-manifest.d3a6b7c99b84947a7cd9d81dd800c5dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e56577a375a6f0112ab0ad3bd8ef59",
+    "url": "/ATGtest/index.html"
+  },
+  {
+    "revision": "4499e9855946684a55c2",
+    "url": "/ATGtest/static/css/main.f11f3894.chunk.css"
+  },
+  {
+    "revision": "24815e48ccc7466c623a",
+    "url": "/ATGtest/static/js/2.a20fc832.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ATGtest/static/js/2.a20fc832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4499e9855946684a55c2",
+    "url": "/ATGtest/static/js/main.ca805e93.chunk.js"
+  },
+  {
+    "revision": "e3d2b4acb97e70677096",
+    "url": "/ATGtest/static/js/runtime-main.48d8c6d5.js"
+  }
+]);
