2195d0d8ba123803ff9db30ae449814ce8667d56
Marten <marten.frisk@gmail.com>
2020-08-11 16:55:58 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,173 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ gameType, setGameType ] = useState(String)
+	const [ selectedRace, setSelectedRace ] = useState({ race: '', time: '' })
+	const [ raceInfo, setRaceInfo ] = useState<emptyRace | undefined>(undefined)
+	const [ gameData, setGameData ] = useState<emptyGame | undefined>(undefined)
+	const [ notAllowed, setNotAllowed ] = useState(false)
+
+	interface emptyGame {
+		betType: string
+		upcoming: [{ id: string; startTime: string }]
+		results: []
+	}
+
+	interface emptyRace {
+		id: string
+		status: string
+		races: [
+			{
+				date: string
+				name: string
+				scheduledStartTime: string
+				starts: [
+					{
+						number: number
+						driver: {
+							firstName: string
+							lastName: string
+						}
+						horse: {
+							name: string
+							trainer: {
+								firstName: string
+								lastName: string
+							}
+							pedigree: {
+								father: {
+									name: string
+								}
+							}
+						}
+					}
+				]
+			}
+		]
+	}
+
+	const allowedTypes = [ 'V75', 'V65', 'V64', 'V4' ]
+
+	useEffect(
+		() => {
+			selectedRace.race !== '' && fetchFromApi('games', selectedRace.race, setRaceInfo)
+		},
+		[ selectedRace ]
+	)
+
+	const handleSubmit = (e: any) => {
+		e.preventDefault()
+		console.log(typeof e)
+		if (allowedTypes.includes(gameType)) {
+			fetchFromApi('products', gameType, setGameData)
+			setNotAllowed(false)
+		} else {
+			setGameData(undefined)
+			setRaceInfo(undefined)
+			setNotAllowed(true)
+		}
+	}
+
+	return (
+		<div className="w-full px-4 md:px-0 text-white md:w-1/2 mx-auto mt-6 md:mt-20 flex items-center flex-col">
+			<h1 className="text-3xl mb-6 italic uppercase text-yellow-500">ATG frontend interview task</h1>
+			<p className="mb-4">Enter a game type below (V75, V65, V64 or V4)</p>
+			<form onSubmit={(e) => handleSubmit(e)} name="inputForm">
+				<input
+					value={gameType}
+					onChange={(e) => setGameType(e.target.value.toUpperCase())}
+					className="text-center text-black p-2"
+					type="text"
+				/>
+				<input type="submit" value="Submit" className="text-gray-900 p-2" />
+			</form>
+			<div className="w-full">
+				{notAllowed && (
+					<p className="bg-red-600 rounded-md mt-6 text-white p-6">
+						Please enter one of the following game types: V75, V65, V64 or V4.
+					</p>
+				)}
+				{gameData !== undefined && (
+					<div className="mt-10 w-full flex flex-wrap md:flex-col">
+						<p className="mb-4">
+
+            Click a race to see detailed information
+            </p>
+            
+            {gameData.upcoming.map((race) => (
+							<div
+								key={race.id}
+								onClick={() => setSelectedRace({ race: race.id, time: race.startTime })}
+								className={`cursor-pointer border-b-4 w-1/2 md:w-40 my-1 p-2 ${race.id === selectedRace.race
+									? 'border-blue-100'
+									: 'border-blue-900 '}`}
+							>
+								&gt; {formatDate(race.startTime)}
+							</div>
+						))}
+					</div>
+				)}
+				{raceInfo !== undefined && (
+					<div className="text-black">
+						<div className="text-center text-white">
+							<div className="mt-12 mb-6 text-2xl">{formatDate(selectedRace.time)}</div>
+							<div>
+								This race has {raceInfo.races.length} starts and is {raceInfo.status}.
+							</div>
+						</div>
+						{raceInfo.races.map((race, index) => (
+							<div className="flex flex-wrap my-10 w-full bg-blue-100 p-4">
+								<div className="w-full text-sm mb-4">Start {index + 1}</div>
+								<div className="w-1/4">
+									{moment(race.scheduledStartTime).format('H[:]mm')} {`(`}
+									{moment(race.date).format('ddd D')}
+									{`)`}
+								</div>
+								<div className="w-3/4 text-lg">{race.name}</div>
+								<div className="w-full" />
+								{race.starts.map((start) => (
+									<div className="w-full flex flex-wrap md:w-1/3 py-4 px-2">
+										<div className="w-1/6">
+											<div className="rounded-full shadow-sm text-center w-6 h-6 bg-white">
+												{start.number}
+											</div>
+										</div>
+                    <div className="w-5/6 flex flex-col">
+
+										<div className="pl-2">
+											&#x1F472; {start.driver.firstName} {start.driver.lastName}
+										</div>
+										<details className="pl-2">
+                      <summary>
+
+                      &#x1F40E; <span className="border-b border-blue-800 border-dotted">{start.horse.name}</span>
+                      </summary>
+												<p className="my-1 text-sm pl-2">
+													Trainer:{' '}
+													{start.horse.trainer.firstName} {start.horse.trainer.lastName}
+												</p>
+												<p className="my-1 text-sm pl-2">Father: {start.horse.pedigree.father.name}</p>
+                      </details>
+                    </div>
+									</div>
+								))}
+							</div>
+						))}
+					</div>
+				)}
+			</div>
+		</div>
+	)
 }
 
-export default App;
+function fetchFromApi(type: string, input: string, state: any) {
+	fetch(`https://www.atg.se/services/racinginfo/v1/api/${type}/${input}`)
+		.then((response) => response.json())
+		.then((result) => state(result))
+		.catch((error) => console.log('error', error))
+}
+
+const formatDate = (date: string) => moment(date).format('ddd Do H[:]mm')
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.html',
+      './src/**/*.tsx',
+      './src/**/*.jsx'
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
